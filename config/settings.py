ac249a882d3bf3c2ace6af412f5ceed6c8d283f1
Punnapa Pramotekul <punnapa.pr@ku.th>
2026-04-09 13:39:50 +0700

    remove suno api token from settings

--- config/settings.py
+++ config/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,4 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'music.User'
 
 GENERATOR_STRATEGY = "mock"  # or "suno"
-SUNO_API_KEY = "a8707e629cd80fccd8c2e624fc63b402"
+load_dotenv()
+SUNO_API_KEY = os.getenv("SUNO_API_KEY")
